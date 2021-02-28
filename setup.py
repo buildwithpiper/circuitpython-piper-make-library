4c8e95155546af118bfe2426efd9a9c9bcbadd10
Matthew Matz <19737272+MatzElectronics@users.noreply.github.com>
2021-02-27 17:57:29 -0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,52 @@
+"""A setuptools based setup module.
+
+See:
+https://packaging.python.org/en/latest/distributing.html
+https://github.com/pypa/sampleproject
+"""
+
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+
+# To use a consistent encoding
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, "README.rst"), encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name="piper-blockly",
+    use_scm_version=True,
+    setup_requires=["setuptools_scm"],
+    description="CircuitPython library to support Block Coding with microcontrollers.",
+    long_description=long_description,
+    long_description_content_type="text/x-rst",
+    # The project's main homepage.
+    url="https://github.com/buildwithpiper/PiperBlocklyLibrary.git",
+    # Author details
+    author="",
+    author_email="",
+    install_requires=[],
+    # Choose your license
+    license="MIT",
+    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: System :: Hardware",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+    ],
+    # What does your project relate to?
+    keywords="blockly coding functions pico hardware micropython circuitpython",
+    # You can just specify the packages manually here if your project is
+    # simple. Or you can use find_packages().
+    py_modules=["piper_blockly"],
+)
